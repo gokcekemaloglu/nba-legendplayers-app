102b798b8ae925f434632831d960740f1c6d19aa
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-07-15 16:15:54 +0300

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import CardContainer from './components/CardContainer';
 import Header from './components/Header';
-import PlayerCard from './components/PlayerCard';
+// import PlayerCard from './components/PlayerCard';
 // import {data} from "./helper/data"
 
 
